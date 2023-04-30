987e7fe1b6b15fa46141cc66648b8b0c8f9c3061
Serhii-Piatak-NBS-Lviv <Serhii.Piatak@ua.nestle.com>
2023-04-30 15:30:49 +0300

    Conflicts in Filter.jsx solved

--- src/features/filter/Filter.jsx
+++ src/features/filter/Filter.jsx
@@ -1,28 +1,15 @@
 import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-<<<<<<< HEAD
-import { cx } from '@emotion/css';
-import useThemify from '../../app/hooks/useThemify';
-=======
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
 import useThemifiedComponent from '../../app/hooks/useThemifiedComponent';
 import restAPI from '../../app/apisimul/filter/name-categories';
 import {
     Drawer,
     DrawerBody,
     DrawerFooter,
-<<<<<<< HEAD
-    DrawerHeader,
-=======
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
     DrawerOverlay,
     DrawerContent,
     DrawerCloseButton,
     Button,
-<<<<<<< HEAD
-    Input,
-=======
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
     useDisclosure
 } from '@chakra-ui/react';
 
@@ -47,98 +34,18 @@ const Filter = () => {
 
     const { t } = useTranslation();
 
-<<<<<<< HEAD
-    const [defaultTheme, overrideTheme, isThemeOverriden] = useThemify('filters');
-    const [bottomFilter, overrideBottomFilter, isBottomFilterThemeOverriden] = useThemify('bottom-filter-wrapper');
-=======
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
     const [cssFiltersContainer] = useThemifiedComponent('filters');
     const [cssBottomFilters] = useThemifiedComponent('bottom-filter-wrapper');
     const [cssDrawerOverlay] = useThemifiedComponent('filter-drawer-overlay');
     const [cssDrawerContent] = useThemifiedComponent('filter-drawer-content');
 
-<<<<<<< HEAD
-    return(
-        <div className={cssFiltersContainer} id="filters">            
-=======
     return (
         <div className={cssFiltersContainer} id="filters">
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
             <SectionHeader
                 title={t('filter header - get inspiration')}
                 subTitle={t('filter subtitle - we compiled a list')}
             />
 
-<<<<<<< HEAD
-        <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
-            Open
-        </Button>
-      <Drawer
-        isOpen={isOpen}
-        placement='top'
-        onClose={onClose}
-        finalFocusRef={btnRef}
-        size='md'
-      >
-        <DrawerOverlay className={cssDrawerOverlay} />
-        <DrawerContent className={cssDrawerContent}>
-          <DrawerCloseButton />
-
-          <DrawerBody>
-
-            <SwiperWithFilters
-                title={t('filter slider title')}
-                restAPI={restAPI}
-            />
-
-            <div className={cssBottomFilters}>
-                <SearchInput 
-                    title={t('filter live search title')}
-                    placeholder={t('filter live search placeholder')}
-                />
-                <GenderSelection 
-                    title={t('filter gender selector title')}
-                    radios={[
-                        t('filter gender female'),
-                        t('filter gender male'),
-                        t('filter gender both')
-                    ]}
-                />
-            </div>
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme='blue'>Save</Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-
-            {/* <SwiperWithFilters
-                title={t('filter slider title')}
-                restAPI={restAPI}
-            />
-            <div className={cx(
-            { [bottomFilter]: true },
-            { [overrideBottomFilter]: isBottomFilterThemeOverriden }
-        )}>
-                <SearchInput 
-                    title={t('filter live search title')}
-                    placeholder={t('filter live search placeholder')}
-                />
-                <GenderSelection 
-                    title={t('filter gender selector title')}
-                    radios={[
-                        t('filter gender female'),
-                        t('filter gender male'),
-                        t('filter gender both')
-                    ]}
-                />
-            </div> */}
-        </div>        
-=======
             <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
                 Open
             </Button>
@@ -185,7 +92,6 @@ const Filter = () => {
                 </DrawerContent>
             </Drawer>
         </div>
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
     )
 };
 
