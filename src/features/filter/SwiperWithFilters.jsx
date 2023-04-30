f58bdd58ff32aad9654852d6d75b7a944261a1d1
Serhii-Piatak-NBS-Lviv <Serhii.Piatak@ua.nestle.com>
2023-04-30 15:38:04 +0300

    Conflicts in SwiperWithFilters.jsx solved

--- src/features/filter/SwiperWithFilters.jsx
+++ src/features/filter/SwiperWithFilters.jsx
@@ -1,35 +1,20 @@
 import { useDispatch } from 'react-redux';
-<<<<<<< HEAD
-import { cx } from '@emotion/css';
-=======
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
 import { Navigation, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 
-<<<<<<< HEAD
-// import useThemify from '../../app/hooks/useThemify';
-=======
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 import './CategorySwiper.css';
 
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
 import useThemifiedComponent from '../../app/hooks/useThemifiedComponent';
 import SwiperItem from './SwiperItem';
 import SelectedFilters from './SelectedFilters';
 import { setSelectedCategory } from './filterSlice';
 
-<<<<<<< HEAD
-const SwiperWithFilters = ({restAPI, title}) => {
-    const dispatch = useDispatch();    
-
-    // const [mainTitle, overrideTitle, isMainTitleOverriden] = useThemify('filters-categories-title');
-    const [cssCategoriesTitle] = useThemifiedComponent('filters-categories-title');
-=======
 const SwiperWithFilters = ({ restAPI, title }) => {
     const dispatch = useDispatch();
 
@@ -42,36 +27,11 @@ const SwiperWithFilters = ({ restAPI, title }) => {
 
     const [cssCategoriesTitle] = useThemifiedComponent('filters-categories-title');
     const [cssSwiperWrapper] = useThemifiedComponent('filters-swiper-wrapper');
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
 
     const handleFilter = (category) => {
         dispatch(setSelectedCategory(category));
     }
 
-<<<<<<< HEAD
-  return (
-    <div>
-        <h3 className={cssCategoriesTitle}> {title} </h3>
-        <Swiper
-            modules={[Navigation, Scrollbar, A11y]}
-            spaceBetween={20}
-            slidesPerView={7}
-            navigation
-            scrollbar={{ draggable: true }}>
-            {
-                restAPI.list.map((category, index) => {
-                    return (
-                        <SwiperSlide key={index}>
-                            <SwiperItem {...category} handleFilter={handleFilter}/>
-                        </SwiperSlide>
-                    )
-                })
-            }       
-            <SelectedFilters handleFilter={handleFilter}/> 
-        </Swiper>
-    </div>    
-  )
-=======
     return (
         <>
             <h3 className={cssCategoriesTitle}>
@@ -120,7 +80,6 @@ const SwiperWithFilters = ({ restAPI, title }) => {
         </>
 
     )
->>>>>>> a42be11c0e3fdd6a83dd0695018fb37ec8d8eebc
 }
 
 export default SwiperWithFilters;
