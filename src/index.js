e8df5231b3af86d11ba73cf73502707cbef3c534
Nazarii Lysak <Nazarii.Lysak@ua.nestle.com>
2023-08-07 14:22:15 +0300

    add spiner and overlay hwihe component is loading

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './i18n/config';
 import store from './app/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import { Loader } from './features/Loader/Loader';
 import './index.css';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 const queryClient = new QueryClient();
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
+const AppWithDelay = () => {
+  const [isComponentLoaded, setIsComponentLoaded] = useState(false);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </Provider>
-  </React.StrictMode>
-);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsComponentLoaded(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          {isComponentLoaded ? <App /> : <Loader />}
+        </QueryClientProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+root.render(<AppWithDelay />);
